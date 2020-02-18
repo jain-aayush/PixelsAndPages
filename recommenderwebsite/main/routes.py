ad942cbdc5397e2bfcd08a1cf376f3fd2ba0c230
Aayush <patni.aayush1998@gmail.com>
2020-02-18 22:39:15 +0530

    Added Documentation for main folder

--- recommenderwebsite/main/routes.py
+++ recommenderwebsite/main/routes.py
@@ -6,11 +6,13 @@ main = Blueprint('main',__name__)
 @main.route("/")
 @main.route("/home")
 def home():
+	""" Route for home page """
 	page = request.args.get('page',1, type=int)
 	books = Book.query.order_by(Book.title).paginate(page = page,per_page = 5)
 	return render_template('home.html')
 
 @main.route("/about")
 def about():
+	""" Route for About Page """
     return render_template('about.html', title='About')
 
